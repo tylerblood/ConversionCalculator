9d292e8d9db183de1884327c1f1b5e5ee06087dc
Tyler Blood <twbcdc@mail.missouri.edu>
2019-05-10 12:49:24 -0500

    UI set up

---
+++ ConversionController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConversionController
+//
+//  Created by Tyler Blood on 5/10/19.
+//  Copyright © 2019 Tyler Blood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
